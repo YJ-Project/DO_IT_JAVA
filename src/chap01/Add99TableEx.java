ff0ffee6f2bb0945a24c98c80605da89da397683
lyj <leeyujin95y@naver.com>
2020-10-20 17:57:34 +0900

    page 38 Q13

---
+++ src/chap01/Add99TableEx.java
@@ -0,0 +1,29 @@
+package chap01;
+/**
+ * DOIT 01.기본알고리즘 연습문제(page 38)
+ * @author lyj
+ *
+ */
+public class Add99TableEx {
+
+	/**
+	 * Q13. 곱셈이 아니라 덧셈을 출력하는 프로그램을 작성하세요.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("   |");
+		for(int i=1;i<=9;i++){
+			System.out.printf("%3d",i);
+		}
+		System.out.println();
+		System.out.println("---+---------------------------");
+		
+		for(int i=1;i<=9;i++){
+			System.out.printf("%2d | ",i);
+			for(int j=1;j<=9;j++){
+				System.out.printf("%3d",i+j);
+			}
+			System.out.println();
+		}
+	}
+}
